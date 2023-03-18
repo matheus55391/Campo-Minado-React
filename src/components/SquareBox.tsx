6b16424fb0df93768b3a8ac47358678e1a16d52b
matheus55391 <matheus.manulo55@gmail.com>
2023-03-18 02:20:08 -0300

    numberValue

--- src/components/SquareBox.tsx
+++ src/components/SquareBox.tsx
@@ -1,32 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 interface SquareBoxProps {
-  isBomb: boolean;
-}
+    isBomb: boolean;
+    numberValue?: number;
+  }
+  
+const SquareBox = ({ isBomb, numberValue }: SquareBoxProps) => {
+    const [isRevealed, setIsRevealed] = useState(false);
 
-const SquareBox = ({ isBomb }: SquareBoxProps) => {
-  const [isRevealed, setIsRevealed] = useState(false);
+    const handleClick = () => {
+        setIsRevealed(true);
+    };
 
-  const handleClick = () => {
-    setIsRevealed(true);
-  };
+    const defaultBoxClasses = 'w-8 h-8 border border-black bg-white cursor-pointer';
+    const revealedBoxClasses = isBomb ? 'bg-red-600' : 'bg-gray-200';
+    const textClasses = 'text-base font-bold';
 
-  const defaultBoxClasses = 'w-8 h-8 border border-black bg-white cursor-pointer';
-  const revealedBoxClasses = isBomb ? 'bg-red-600' : 'bg-gray-200';
-  const textClasses = 'text-base font-bold';
-
-  return (
-    <div
-      className={`${defaultBoxClasses} ${isRevealed ? revealedBoxClasses : ''}`}
-      onClick={handleClick}
-    >
-      {isRevealed && (
-        <div className="w-full h-full flex items-center justify-center">
-          <span className={textClasses}>{isBomb ? 'B' : '0'}</span>
+    return (
+        <div
+        className={`${defaultBoxClasses} ${isRevealed ? revealedBoxClasses : ''}`}
+        onClick={handleClick}
+        >
+        {isRevealed && (
+            <div className="w-full h-full flex items-center justify-center">
+            <span className={textClasses}>
+                {isBomb ? 'B' : numberValue !== undefined ? numberValue : '0'}
+            </span>
+            </div>
+        )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
-export default SquareBox;
+export default SquareBox
